1e2441cec087d6290cb4b93a6ef0cee2d35544a0
muchira007 <muchirasteve2@gmail.com>
2023-10-27 10:39:59 -0700

    error sorting

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
-#include <stdio.h>		
+#include <stdio.h>
 #include "main.h"
 /**
- * main - prints the name of the program		
+ * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return: Always 0 (Success)		
+ * Return: Always 0 (Success)
  */
-int main(int argc __attribute__((unused)), char *argv[])		
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv);		
+	printf("%s\n", *argv);
 	return (0);
 }
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,14 +1,13 @@
-
 #include <stdio.h>
-	#include <stdlib.h>
-	#include <ctype.h>
-	#include <string.h>
-	/**
-	 * check_num - check - string there are digit
-	 * @str: array str
-	 *
-	 * Return: Always 0 (Success)
-	 */
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
 	int check_num(char *str)
 	{
 		/*Declaring variables*/
@@ -32,13 +31,13 @@
 	}
 
 
-	/**
-	 * main - Print the name of the program
-	 * @argc: Count arguments
-	 * @argv: Arguments
-	 *
-	 * Return: Always 0 (Success)
-	 */
+/**
+ * main - Print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
 
 
 	int main(int argc, char *argv[])
