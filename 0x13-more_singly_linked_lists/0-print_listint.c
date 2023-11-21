9ec5d90aed60f687c5c694c514176c31ec836948
muchira007 <muchirasteve2@gmail.com>
2023-11-21 03:55:24 -0800

    error handling

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked lists
- * @h: linked list
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
  *
- * Return: numberof nodes
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num  = 0;
+	size_t num = 0;
 
 	while (h)
 	{
-		printf("%d \n:", h->n);
+		printf("%d\n", h->n);
 		num++;
 		h = h->next;
 	}
+
 	return (num);
 }
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 
 /**
- * listint_len - print out number of elements in h
- * @h: linked list
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
  *
  * Return: number of nodes
  */
@@ -10,10 +10,11 @@ size_t listint_len(const listint_t *h)
 {
 	size_t num = 0;
 
-	while h
+	while (h)
 	{
 		num++;
 		h = h->next;
 	}
+
 	return (num);
 }
